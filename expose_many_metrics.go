2a5b84e7d4eb13e8c89ede99730a28cf2ef26e47
Brian Brazil <brian.brazil@robustperception.io>
2019-11-15 11:38:57 +0000

    Adjust to have values more similar to latency metrics.

--- expose_many_metrics.go
+++ expose_many_metrics.go
@@ -24,12 +24,19 @@ var (
 type collector struct {
 }
 
+func randFloat() float64 {
+  // Simulate nanosecond precision latency around 1s.
+  r := rand.NormFloat64() + 1;
+  r *= 1e9;
+  return math.Round(r) / 1e9;
+}
+
 func (collector) Describe(ch chan<- *prometheus.Desc) {
   metric.Describe(ch)
 }
 func (collector) Collect(ch chan<- prometheus.Metric) {
 	for i := 0; i < *numTimeseries; i++ {
-		metric.WithLabelValues(fmt.Sprintf("labelabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghij%d", i)).Add(math.Max(0, rand.NormFloat64()))
+		metric.WithLabelValues(fmt.Sprintf("labelabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghij%d", i)).Add(math.Max(0, randFloat()))
 	}
   metric.Collect(ch)
 }
